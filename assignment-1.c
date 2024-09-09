81352b32d293bb1db598f59242b71481fa3280db
hs5385 <hs5385@srmist.edu.in>
2024-09-09 08:18:46 +0530

    assignment-1.c

--- assignment-1.c
+++ assignment-1.c
@@ -11,11 +11,14 @@ void fun()
     printf("Register variable : %d\n" , register_var);
     printf("Global variable :   %d\n", global_var);
     printf("Static Globlal variable:   %d\n" , static_global_var);
-    a= a+40;
+    a=a+40;
     b=b+30;
 }
 int main()
 {
+    printf("Name : Harsh Soni\n");
+    printf("Class : CSE-AI&ML AG2 section\n");
+    printf("Reg. No. : RA2411026010878");
     printf("First call :\n ");
     fun();
     printf("Second call ;\n");
